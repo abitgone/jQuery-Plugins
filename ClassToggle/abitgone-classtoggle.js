00ed2c177debc4160f0a571b6b8be11a6dd2f749
Anthony Williams <anthony@abitgone.co.uk>
2012-08-08 15:34:15 +0100

    Revert "Fix $sender for browsers that don't support window.event"
    This reverts commit d0035235f514daddde836b794cca0e366b7e647b.

--- ClassToggle/abitgone-classtoggle.js
+++ ClassToggle/abitgone-classtoggle.js
@@ -1,132 +1,133 @@
-/*    
-    abitgone-classtoggle.js
-    =======================
-    ClassToggle toggles, on a target element, a single class on and off or between two classes.
-    
-    Usage
-    -----    
-    As a bare minimum, your trigger element must have the following attributes:
-    
-    -   **href** (for anchors) or **data-classtoggle-target**  
-        This specifies the target element
-    
-    -   **data-classtoggle-class**  
-        The class to be toggled, on and off, by the trigger
-    
-    Additionally, you may specify the following additional class names:
-    
-    -   **data-classtoggle-altclass**  
-        The class you'd like to alternate with the main class. For example, data-classtoggle-class="Red"
-        and data-classtoggle-altclass="Green" will alternate the .Red and .Green classes each time the
-        trigger element is activated.
-    
-    -   **data-classtoggle-trigger-activeclass**  
-        Specifies a class to be added to the trigger when it activates the ClassToggle.
-    
-    -   **data-classtoggle-trigger-selector**
-        Specifies a jQuery selector which data-classtoggle-trigger-activeclass will be toggled on when
-        the trigger elements are activated. Useful if you have more than one trigger which could
-        trigger the target element.
-    
-    For more information on how to use this plugin, visit http://abitgone.github.com/jQuery-Plugins/ClassToggle    
-*/
+/*	
+ 	abitgone-classtoggle.js
+	=======================
+  	ClassToggle toggles, on a target element, a single class on and off or between two classes.
+ 
+ 	Usage
+	-----	
+	As a bare minimum, your trigger element must have the following attributes:
+ 
+ 	- 	**href** (for anchors) or **data-classtoggle-target**  
+		This specifies the target element
+	
+	-	**data-classtoggle-class**  
+		The class to be toggled, on and off, by the trigger
+		
+	Additionally, you may specify the following additional class names:
+ 
+ 	-	**data-classtoggle-altclass**  
+		The class you'd like to alternate with the main class. For example, data-classtoggle-class="Red"
+		and data-classtoggle-altclass="Green" will alternate the .Red and .Green classes each time the
+		trigger element is activated.
+ 
+	-	**data-classtoggle-trigger-activeclass**  
+		Specifies a class to be added to the trigger when it activates the ClassToggle.
+		
+	-	**data-classtoggle-trigger-selector**
+		Specifies a jQuery selector which data-classtoggle-trigger-activeclass will be toggled on when
+		the trigger elements are activated. Useful if you have more than one trigger which could
+		trigger the target element.
+ 
+	For more information on how to use this plugin, visit http://abitgone.github.com/jQuery-Plugins/ClassToggle
+ 
+ */
+ 
 !function (jQuery) {
-    
-    // ClassToggle Public Class Definition
-    
-    var ClassToggle = function (element, options) {
-        
-        this.$element = $(element)
-        this.options = $.extend({}, $.fn.classToggle.defaults, options)
-        
-        if (this.options.parent) {
-          this.$parent = $(this.options.parent)
-        }
-        
-        this.options.classToggle && this.classToggle()
-        
-    }
-    
-    ClassToggle.prototype = {
-        
-        constructor: ClassToggle,
-        
-        classToggle: function (option) {
-            
-            sender = typeof(event) != 'undefined' ? (event ? event.target : window.event.srcElement) : this.options.sender;
-            $sender = $(sender);
-            
-            var tcClass = this.options.classtoggleClass; // $sender.attr('data-classtoggle-class');
-            var tcClassAlt = this.options.classtoggleAltclass; //$sender.attr('data-classtoggle-altclass');
-            var tcTriggerClass = this.options.classtoggleTriggerActiveclass; // $sender.attr('data-classtoggle-trigger-activeclass');
-            var tcTriggerSelector = this.options.classtoggleTriggerSelector; // $sender.attr('data-classtoggle-trigger-selector');
-            var target = this.options.classtoggleTarget; // $sender.attr('data-classtoggle-target');
-            
-            if (target == undefined) {
-                target = ($sender.attr('href') == undefined ? $sender.attr('href') : $sender[0].href);
-                target = target.replace(/.*(?=#[^\s]+$)/, ''); // Strip for IE7
-            }
-            if (target == undefined) return;
-            var $target = $(target);
-            
-            if (tcClass == undefined || $target == undefined) return;
-            
-            var targetMain = $target.hasClass(tcClass);    
-            var targetAlt = tcClassAlt == undefined ? false : $target.hasClass(tcClassAlt);
-            
-            if (tcClassAlt == undefined) {
-                $target.toggleClass(tcClass);
-            } else {
-                if((targetMain && targetAlt) || (!targetMain && !targetAlt)) {
-                    $target.toggleClass(tcClassAlt);
-                } else {
-                    $target.toggleClass(tcClassAlt);
-                    $target.toggleClass(tcClass);                    
-                }
-            }
-            
-            if (tcTriggerClass == undefined) return;
-            
-            var $triggers;        
-            if (tcTriggerSelector == undefined) {
-                $triggers = $sender;
-            } else {
-                $triggers = $(tcTriggerSelector)
-            }
-            $triggers.toggleClass(tcTriggerClass);
-            
-        }
-    }
-    
-    // ClassToggle Plugin definition
-    $.fn.classToggle = function(option) {
-        return this.each(function (e) {
-            var $this = $(this),
-                data = $this.data('classToggle'),
-                options = typeof option == 'object' && option;
-                if (!data) $this.data('classToggle', (data = new ClassToggle(this, options)));
-            if (typeof option == 'string') data[option]();
-        });
-    }
-    
-    $.fn.classToggle.defaults = {
-        classToggle: true
-    };
-    
-    $.fn.classToggle.Constructor = ClassToggle;
-    
-    // ClassToggle Data-Api
-    $(function (){
-        $('body').on('click.classtoggle.data-api', '[data-classtoggle-class]', function (e) { 
-            var $this = $(this)
-              , href
-              , target = $this.attr('data-classtoggle-target')
-                || e.preventDefault()
-                || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '') // Strip for IE7        
-              , option = $(target).data('classToggle') ? 'classToggle' : $this.data();
-            option.sender = e.target;
-            $(target).classToggle(option);
-        })    
-    });
-    
+
+	// ClassToggle Public Class Definition
+
+	var ClassToggle = function (element, options) {
+
+	    this.$element = $(element)
+	    this.options = $.extend({}, $.fn.classToggle.defaults, options)
+
+	    if (this.options.parent) {
+	      this.$parent = $(this.options.parent)
+	    }
+
+	    this.options.classToggle && this.classToggle()
+
+	}
+	
+	ClassToggle.prototype = {
+		
+		constructor: ClassToggle,
+		
+		classToggle: function (option) {
+			
+			var sender = event.target ? event.target : window.event.srcElement;
+			$sender = $(sender);
+							
+			var tcClass = $sender.attr('data-classtoggle-class');
+			var tcClassAlt = $sender.attr('data-classtoggle-altclass');
+			var tcTriggerClass = $sender.attr('data-classtoggle-trigger-activeclass');
+			var tcTriggerSelector = $sender.attr('data-classtoggle-trigger-selector');
+			var target = $sender.attr('data-classtoggle-target');
+			
+			if (target == undefined) {
+				target = $sender.attr('href') == undefined ? $sender.attr('href') : $sender[0].href;
+				target = target.replace(/.*(?=#[^\s]+$)/, ''); // Strip for IE7
+			}
+			if (target == undefined) return;
+			var $target = $(target);
+
+			if (tcClass == undefined || $target == undefined) return;
+			
+			var targetMain = $target.hasClass(tcClass);	
+			var targetAlt = tcClassAlt == undefined ? false : $target.hasClass(tcClassAlt);
+										
+			if (tcClassAlt == undefined) {
+				$target.toggleClass(tcClass);
+			} else {
+				if((targetMain && targetAlt) || (!targetMain && !targetAlt)) {
+					$target.toggleClass(tcClassAlt);
+				} else {
+					$target.toggleClass(tcClassAlt);
+					$target.toggleClass(tcClass);					
+				}
+			}
+			
+			if (tcTriggerClass == undefined) return;
+
+			var $triggers;		
+			if (tcTriggerSelector == undefined) {
+				$triggers = $sender;
+			} else {
+				$triggers = $(tcTriggerSelector)
+			}
+			$triggers.toggleClass(tcTriggerClass);
+			
+		}
+	}
+
+	// ClassToggle Plugin definition
+	$.fn.classToggle = function(option) {
+		return this.each(function () {
+			var $this = $(this),
+				data = $this.data('classToggle'),
+				options = typeof option == 'object' && option;
+				if (!data) $this.data('classToggle', (data = new ClassToggle(this, options)));
+			if (typeof option == 'string') data[option]();
+		});
+	}
+			
+	$.fn.classToggle.defaults = {
+		classToggle: true
+	};
+	
+	$.fn.classToggle.Constructor = ClassToggle;
+
+	// ClassToggle Data-Api
+	$(function (){
+		$('body').on('click.classtoggle.data-api', '[data-classtoggle-class]', function (e) { 
+			var $this = $(this);
+			var href;
+			var target = $this.attr('data-classtoggle-target') || 
+						 (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, ''); // Strip for IE7		
+			var option = $(target).data('classToggle') ? 'classToggle' : $this.data();
+			e.preventDefault();
+			$(target).classToggle(option);
+		})	
+	});
+	
 }(window.jQuery);
