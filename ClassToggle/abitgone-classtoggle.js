7366704243bda288986902769576368b8da415aa
Anthony Williams <anthony@abitgone.co.uk>
2013-05-01 12:15:12 +0100

    ClassToggle: Add ability to explicitly add/remove classes
    ClassToggle can now explicitly add or remove classnames from target
    elements. To do this, simply prepend -- or ++ to explicitly
    remove or add class names to the target element(s). For example, to
    toggle one class whilst explicitly removing another, use:
    
    data-classtoggle-class="toggle-this,--remove-this"
    
    To remove one class whilst adding another, use:
    
    data-classtoggle-cass="++add-this,--remove-this"

--- ClassToggle/abitgone-classtoggle.js
+++ ClassToggle/abitgone-classtoggle.js
@@ -9,25 +9,24 @@
     
     -   **href** (for anchors) or **data-classtoggle-target**  
         This specifies the target element
-        
+    
     -   **data-classtoggle-class**  
         The class, or comma-separated classes, to be toggled, on and off, by the trigger
-        
+    
     Additionally, you may specify the following additional class names:
     
     -   **data-classtoggle-altclass**  
         The class you'd like to alternate with the main class. For example, data-classtoggle-class="Red" and 
         data-classtoggle-altclass="Green" will alternate the .Red and .Green classes each time the trigger element 
-        is activated. Unlike multiple classes, this will check for the existence of data-classtoggle-class and
-        data-classtoggle-altclass before toggling the main class or alternate class.
-        
+        is activated.
+    
     -   **data-classtoggle-trigger-activeclass**  
         Specifies a class to be added to the trigger when it activates the ClassToggle.
-        
+    
     -   **data-classtoggle-trigger-selector**  
         Specifies a jQuery selector which data-classtoggle-trigger-activeclass will be toggled on when the trigger 
         elements are activated. Useful if you have more than one trigger which could trigger the target element.
-        
+    
     For more information on how to use this plugin and for live examples, visit 
     http://abitgone.github.com/jQuery-Plugins/ClassToggle
     
@@ -35,23 +34,23 @@
     -------
     Author: Anthony Williams
     Web: http://abitgone.github.com/jQuery-Plugins/ClassToggle
-    
+
     Copyright (c) 2012 Anthony Williams
-    
+
     Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
     documentation files (the "Software"), to deal in the Software without restriction, including without limitation the 
     rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
     permit persons to whom the Software is furnished to do so, subject to the following conditions:
-    
+
     The above copyright notice and this permission notice shall be included in all copies or substantial portions of
     the Software.
-    
+
     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
     THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
     TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
     THE SOFTWARE.
-    
+
 */
 !function (jQuery) {
     // ClassToggle Public Class Definition
@@ -100,17 +99,13 @@
             var targetAlt = tcClassAlt == undefined ? false : $tcTarget.hasClass(tcClassAlt);
 
             if (tcClassAlt == undefined) {
-                for (var i=0;i<tcClass.length;i++) {
-                    $tcTarget.toggleClass(tcClass[i]);
-                }
+                this.toggleClasses($tcTarget, tcClass);
             } else {
                 if ((targetMain && targetAlt) || (!targetMain && !targetAlt)) {
                     $tcTarget.toggleClass(tcClassAlt);
                 } else {
                     $tcTarget.toggleClass(tcClassAlt);
-                    for (var i=0;i<tcClass.length;i++) {
-                        $tcTarget.toggleClass(tcClass[i]);
-                    }
+                    this.toggleClasses($tcTarget, tcClass);
                 }
             }
 
@@ -124,6 +119,23 @@
             }
             $tcTriggers.toggleClass(tcTriggerClass);
 
+        },
+
+        toggleClasses: function($element, tcClassList) {
+            for (var i=0;i<tcClassList.length;i++) {
+                var match = tcClassList[i].match(/([-+]{2})?(\S+)/);
+                switch(match[1]) {
+                    case '--':
+                        $element.removeClass(match[2]);
+                        break;
+                    case '++':
+                        $element.addClass(match[2]);
+                        break;
+                    default:
+                        $element.toggleClass(match[2]);
+                        break;
+                }
+            }
         }
 
     }
